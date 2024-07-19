1611a16ea19642fc9d951978a1ef92fa61d97a2a
Zack Njine Wachira <zackwachira45@gmail.com>
2024-07-19 23:59:30 +0300

    commit

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+}
+
+function renderBooks(books) {
+  const main = document.querySelector('main')
+  books.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
